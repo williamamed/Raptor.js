a8965c3d8724d0cc8d2b03d4d4a1df295dd0e8c3
William Amed <william.tamayo@ltu.jovenclub.cu>
2018-07-28 09:45:52 -0400

    Update GenericList.js

--- src/Raptorjs/RaptorNode/Resources/genModel/js/app/view/GenericList.js
+++ src/Raptorjs/RaptorNode/Resources/genModel/js/app/view/GenericList.js
@@ -9,6 +9,24 @@ Ext.define('Generate.view.GenericList', {
         this.columns=[{
         	header: 'Tabla', width: 200, flex: 1, dataIndex: 'name'
         }]
+        this.dockedItems = [{
+                dock: 'top',
+                xtype: 'toolbar',
+                items: [{
+                        xtype:'fieldcontainer',
+                        defaultType: 'checkboxfield',
+                        items:[{
+                                    boxLabel  : 'Timestamps',
+                                    inputValue: '1',
+                                    id        : 'checkbox1'
+                                }, {
+                                    boxLabel  : 'Underscored',
+                                    inputValue: '2',
+                                    checked   : true,
+                                    id        : 'checkbox2'
+                                }]
+                    }]
+            }];
         this.selModel=Ext.create('Ext.selection.CheckboxModel', {mode: 'MULTI'});
         this.callParent();
     }
